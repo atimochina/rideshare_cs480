ecc995fdb51d976db2461044a9ca5ef965336061
ang timochina <ang.timochina@gmail.com>
2020-11-29 10:40:40 -0600

    fixed cleaning script to include ride ids

--- cleaning_script.sql
+++ cleaning_script.sql
@@ -1,14 +1,17 @@
 USE rideshare;
 # ---- uncomment line below after first run
-DROP TABLE january_green_clean;
+#DROP TABLE jan_green_one_rider;
+#DROP TABLE jan_green_two_rider;
+
 
 CREATE TABLE jan_green_one_rider
-SELECT pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
+SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
 FROM rideshare.january_green
 WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1;
 
 CREATE TABLE jan_green_two_rider
-SELECT pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
+SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
 FROM rideshare.january_green
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-ORDER BY pickup_datetime;
+WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2;
+
+
