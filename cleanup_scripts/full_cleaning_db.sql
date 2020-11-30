898fc4bbb16baa7f184c56936801e9f88b0db776
ang timochina <ang.timochina@gmail.com>
2020-11-30 07:18:16 -0600

    removed certain files, fixed comments/naming

--- cleanup_scripts/full_cleaning_db.sql
+++ cleanup_scripts/full_cleaning_db.sql
@@ -1,35 +1,69 @@
 USE rideshare;
 
-# add the rider_id column
-ALTER TABLE green_2_february ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+/* Drop statements for tables. If reruning the script uncomment */
+/*
+DROP TABLE rideshare.jan_green_one_rider;
+DROP TABLE rideshare.jan_green_two_rider;
+DROP TABLE rideshare.feb_green_one_rider;
+DROP TABLE rideshare.feb_green_two_rider;
+DROP TABLE rideshare.mar_green_one_rider;
+DROP TABLE rideshare.mar_green_two_rider;
+*/
+# add the ride_id column - do once - uncomment/ comment your section below based on your
+# main table names
 
+# one set of table names- reem
+# ALTER TABLE green_1_january ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ALTER TABLE green_2_february ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ALTER TABLE green_3_march ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+
+# other set of table names -angy, eldin?
+# ALTER TABLE january_green ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ALTER TABLE february_green ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ALTER TABLE march_green ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+
+# same thing uncomment/ comment your FROM statement based on your table names
 # get the single riders
 CREATE TABLE jan_green_one_rider
 SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
-FROM rideshare.green_1_january
+# FROM rideshare.green_1_january 
+FROM rideshare.january_green
 WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
-AND trip_distance > 0;
+AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
 
 # get the double riders
 CREATE TABLE jan_green_two_rider
 SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
-FROM rideshare.green_1_january
+# FROM rideshare.green_1_january
+FROM rideshare.january_green
 WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-AND trip_distance > 0;
-
-# repeat for each database
-ALTER TABLE green_2_february ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
 
 CREATE TABLE feb_green_one_rider
 SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
-FROM rideshare.green_2_february
+# FROM rideshare.green_2_january
+FROM rideshare.february_green
 WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
-AND trip_distance > 0;
+AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
 
 CREATE TABLE feb_green_two_rider
 SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
-FROM rideshare.green_2_february
+# FROM rideshare.green_2_january
+FROM rideshare.february_green
 WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-AND trip_distance > 0;
+AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
 
+CREATE TABLE mar_green_one_rider
+SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
+# FROM rideshare.march_3_january
+FROM rideshare.march_green
+WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
+AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
+
+CREATE TABLE mar_green_two_rider
+SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance
+# FROM rideshare.march_3_january
+FROM rideshare.march_green
+WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
+AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
 
--- testing_queries.sql
+++
@@ -1,8 +0,0 @@
-# Filter out passengers only keep 1 or 2
-CREATE TABLE testing_table AS
-SELECT * 
-FROM april_green
-WHERE passenger_count = 1 ;
-
-SELECT * 
-FROM testing_table;
