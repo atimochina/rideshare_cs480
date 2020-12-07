5526ae14654c841a60d67409393bbfd3318a5fb9
reemhussein <reemyhussein@gmail.com>
2020-12-07 13:53:43 -0600

    updated cleanup script to clean it

--- clean_up_script.sql
+++ clean_up_script.sql
@@ -1,7 +1,8 @@
 USE rideshare;
 
-/* Drop statements for tables. If reruning the script uncomment */
-/*
+#	IF THIS CODE WAS ALREADY RUN, YOU NEED TO DROP THE TABLES FIRST
+#	UNCOMMENT THIS IF YOU NEED TO REDO THE QUERIES
+/*	
 DROP TABLE rideshare.jan_green_one_rider;
 DROP TABLE rideshare.jan_green_two_rider;
 DROP TABLE rideshare.jan_yellow_one_rider;
@@ -13,95 +14,113 @@ DROP TABLE rideshare.feb_yellow_one_rider;
 DROP TABLE rideshare.feb_yellow_two_rider;
 */
 
-# add the ride_id column - do once - uncomment/ comment your section below based on your
-# main table names
+#	IF THIS CODE WAS ALREADY RUN, YOU NEED TO DROP THE TABLES FIRST
+#	UNCOMMENT THIS IF YOU NEED TO REDO THE QUERIES
+#	USED TO ADD A PRIMARY ID TO OUR ORIGINAL DATABASES
 
-# one set of table names- reem
+/*
 # ALTER TABLE green_1_january ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
 # ALTER TABLE green_2_february ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
-# ALTER TABLE green_3_march ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ALTER TABLE yellow1_january ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ALTER TABLE yellow_2_february ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+*/
 
-# other set of table names -angy, eldin?
-# ALTER TABLE january_green ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
-# ALTER TABLE february_green ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
-# ALTER TABLE march_green ADD ride_id INT AUTO_INCREMENT PRIMARY KEY;
+# ---------------- GETTING ALL SINGLE AND DOUBLE RIDERS FOR JANUARY ---------------- 
 
-# same thing uncomment/ comment your FROM statement based on your table names
-# get the single riders
+# SINGLE RIDERS GREEN JANUARY
 CREATE TABLE jan_green_one_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_1_january 
-FROM rideshare.january_green
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.green_1_january 
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 1
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+
 ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
-# get the double riders
+# DOUBLE RIDERS GREEN JANUARY
 CREATE TABLE jan_green_two_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_1_january
-FROM rideshare.january_green
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
-ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.green_1_january
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 2
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
-# same thing uncomment/ comment your FROM statement based on your table names
-# get the single riders
+# SINGLE RIDERS YELLOW JANUARY
 CREATE TABLE jan_yellow_one_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_1_january 
-FROM rideshare.january_yellow
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
-ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.yellow_1_january 
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 1
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
-# get the double riders
+# DOUBLE RIDERS YELLOW JANUARY
 CREATE TABLE jan_yellow_two_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_1_january
-FROM rideshare.january_yellow
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
-ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.yellow_1_january
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 2
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
+# ---------------- GETTING ALL SINGLE AND DOUBLE RIDERS FOR FEBRUARY ---------------- 
+
+# SINGLE RIDERS GREEN FEBRUARY
 CREATE TABLE feb_green_one_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_2_january
-FROM rideshare.february_green
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.green_2_february
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 1
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
+# DOUBLE RIDERS GREEN FEBRUARY
 CREATE TABLE feb_green_two_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_2_january
-FROM rideshare.february_green
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.green_2_february
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 2
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
+# SINGLE RIDERS YELLOW FEBRUARY
 CREATE TABLE feb_yellow_one_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_2_january
-FROM rideshare.february_yellow
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 1
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.yellow_2_february
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 1
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
+# DOUBLE RIDERS YELLOW FEBRUARY
 CREATE TABLE feb_yellow_two_rider
-SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
-get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
-# FROM rideshare.green_2_january
-FROM rideshare.february_yellow
-WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND dropoff_longitude != 0 AND passenger_count = 2
-AND trip_distance > 0 AND pickup_datetime != dropoff_datetime;
+	SELECT ride_id, pickup_datetime, dropoff_datetime, pickup_longitude, pickup_latitude, 
+		dropoff_longitude, dropoff_latitude, passenger_count, trip_distance,
+		get_speed(pickup_datetime, dropoff_datetime, trip_distance) AS 'speed'
+	FROM rideshare.yellow_2_february
+	WHERE pickup_latitude != 0 AND pickup_longitude != 0 AND dropoff_latitude != 0 AND 
+		dropoff_longitude != 0 AND passenger_count = 2
+		AND trip_distance > 0 AND pickup_datetime != dropoff_datetime
+	ORDER BY pickup_datetime, pickup_latitude, pickup_longitude;
 
 
 # MERGING THE DATABASES TO ADD YELLOW AND GREEN TAXIS TOGETHER 
+
 # all january single riders from green and yellow taxis
 CREATE TABLE jan_single_riders
 SELECT * FROM rideshare.jan_green_one_rider
@@ -125,6 +144,3 @@ CREATE TABLE feb_double_riders
 SELECT * FROM rideshare.feb_green_two_rider
 UNION
 SELECT * FROM rideshare.feb_yellow_two_rider;
-
-
-
---
+++ full_algorthim.sql
@@ -0,0 +1,52 @@
+USE rideshare;
+
+# The above function works and is used in full_cleaning_db to add speed component
+#DROP TABLE ride_one;
+
+CREATE TABLE ride_one
+SELECT * FROM jan_single_riders;
+
+#SELECT * FROM ride_one;
+
+#DROP TABLE ride_one_combo;
+CREATE TABLE ride_one_combo
+SELECT r1.ride_id as ride_one_id, r2.ride_id as ride_two_id, TIMESTAMPDIFF(SECOND, r1.pickup_datetime , r2.pickup_datetime) AS time_diff, (r1.speed+r2.speed)/2 as speed,
+r1.trip_distance + r2.trip_distance as original_distance,
+get_distance_mi (r1.pickup_latitude, r1.pickup_longitude, r2.pickup_latitude, r2.pickup_longitude) AS distance_pickup,
+get_distance_mi (r1.pickup_latitude, r1.pickup_longitude, r1.dropoff_latitude, r1.dropoff_longitude) AS middle_o1d1,
+get_distance_mi (r1.pickup_latitude, r1.pickup_longitude, r2.dropoff_latitude, r2.dropoff_longitude) AS middle_o1d2,
+get_distance_mi (r2.pickup_latitude, r2.pickup_longitude, r1.dropoff_latitude, r1.dropoff_longitude) AS middle_o2d1,
+get_distance_mi (r2.pickup_latitude, r2.pickup_longitude, r2.dropoff_latitude, r2.dropoff_longitude) AS middle_o2d2,
+get_distance_mi (r1.dropoff_latitude, r1.dropoff_longitude, r2.dropoff_latitude, r2.dropoff_longitude) AS distance_dropoff
+FROM jan_double_riders r2, ride_one r1
+WHERE TIMESTAMPDIFF(SECOND, r1.pickup_datetime , r2.pickup_datetime) > 0 AND TIMESTAMPDIFF(SECOND, r1.pickup_datetime , r2.pickup_datetime) < 5*60;
+
+#SELECT * FROM rideshare.ride_one_combo;
+
+#drop table ride_combo;
+CREATE TABLE ride_combo
+SELECT ride_one_id, ride_two_id, speed,
+		distance_pickup + distance_dropoff + get_min_middle(middle_o1d1, middle_o1d2, middle_o2d1, middle_o2d2) as total_distance,
+        original_distance - (distance_pickup + distance_dropoff + get_min_middle(middle_o1d1, middle_o1d2, middle_o2d1, middle_o2d2)) as distance_savings,
+        (original_distance - (distance_pickup + distance_dropoff + get_min_middle(middle_o1d1, middle_o1d2, middle_o2d1, middle_o2d2)))/original_distance*100 as percent_savings
+FROM ride_one_combo
+WHERE original_distance - (distance_pickup + distance_dropoff + get_min_middle(middle_o1d1, middle_o1d2, middle_o2d1, middle_o2d2)) > 0;
+
+select * from ride_combo;
+
+#drop table ride_combo_two;
+
+create table ride_combo_two
+select distinct * 
+from ride_combo 
+GROUP BY ride_one_id 
+HAVING percent_savings = max(percent_savings)
+order by ride_one_id, percent_savings DESC;
+
+select distinct * 
+from ride_combo_two
+GROUP BY ride_two_id
+HAVING percent_savings = max(percent_savings);
+
+select avg(percent_savings) from ride_combo_two;
+select SUM(distance_savings) from ride_combo_two;
